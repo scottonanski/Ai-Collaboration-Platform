dda48a0c0fe2b069a44394679790aa9e37d7b7a2
Scott O'Nanski <s.onanski@gmail.com>
2025-04-21 11:19:42 -0600

    Deleted test.tsx files... Will replace them later. Maybe

--- src/types/popover.d.ts
+++
@@ -1,9 +0,0 @@
-import { ButtonHTMLAttributes } from 'react';
-
-declare module 'react' {
-  interface ButtonHTMLAttributes<T> extends React.HTMLAttributes<T> {
-    popovertarget?: string;
-  }
-}
-
-export {};
